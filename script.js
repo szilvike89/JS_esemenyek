612f2905a4b221c319cf36ec5681793ba4003bf4
szilvike89 <szilvike@gmail.com>
2021-03-23 19:29:12 +0100

    1. feladat

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 /*
 1. feladat: Egy gombra kattintva kérd be a felhasználó nevét egy
  felugró ablakban, majd írd ki egy p tag-be, 
- üdvözöllek + neve.
-2. feladat: Illessz be egy tetszőleges képet, és hozz létre 
-egy eseményt.
-3. feldat: Az oldal betöltését ellenőrizd, és üzenj a konzolon,
- ha betöltött.
-4. feladat: Illessz be egy Lorem div-et és változtass meg 3 
+ üdvözöllek + neve.*/
+
+ function myFunction()
+        {
+    let neve=prompt("Kérlek add meg a neved:");
+    document.getElementById("first").innerHTML=("Üdvözöllek "+neve);
+        }
+
+/*2. feladat: Illessz be egy tetszőleges képet, és hozz létre 
+egy eseményt.*/
+
+
+
+/*3. feldat: Az oldal betöltését ellenőrizd, és üzenj a konzolon,
+ ha betöltött.*/
+
+
+
+/*4. feladat: Illessz be egy Lorem div-et és változtass meg 3 
 tulajdonságot JS-ből.*/
+
+
+
 
