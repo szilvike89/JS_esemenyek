ac094dcbabc7890feadaa96263babbb5025b3358
szilvike89 <szilvike@gmail.com>
2021-03-23 18:42:14 +0100

    html es js letrehozasa

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+1. feladat: Egy gombra kattintva kérd be a felhasználó nevét egy
+ felugró ablakban, majd írd ki egy p tag-be, 
+ üdvözöllek + neve.
+2. feladat: Illessz be egy tetszőleges képet, és hozz létre 
+egy eseményt.
+3. feldat: Az oldal betöltését ellenőrizd, és üzenj a konzolon,
+ ha betöltött.
+4. feladat: Illessz be egy Lorem div-et és változtass meg 3 
+tulajdonságot JS-ből.*/
+
