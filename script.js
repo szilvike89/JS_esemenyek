39d0ac8eb75eee49cc7aa6ee2ace9ec90bc0ec3a
szilvike89 <szilvike@gmail.com>
2021-03-25 16:53:47 +0100

    4. feladat

--- script.js
+++ script.js
@@ -12,17 +12,29 @@
 /*2. feladat: Illessz be egy tetszőleges képet, és hozz létre 
 egy eseményt.*/
 
+function kep(){
+    
+}
         
 
 /*3. feldat: Az oldal betöltését ellenőrizd, és üzenj a konzolon,
  ha betöltött.*/
 
- function myFunction2(){
+ function myFunction2()
+{
     console.log("Betöltött az oldal");
 }
 
 /*4. feladat: Illessz be egy Lorem div-et és változtass meg 3 
 tulajdonságot JS-ből.*/
+
+let lorem = document.querySelector("#lorem");
+
+lorem.style.backgroundColor = "#66ff33";
+lorem.style.padding = "20px";
+lorem.style.textDecoration = "underline";
+
+
 
 
 
